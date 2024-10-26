ac1c79be178e3eab2ec13b3fdc3b9a96bd4565c0
TehreemFarooqi-25 <221059@students.au.edu.pk>
2024-10-26 18:59:32 +0500

    changed in src/app/.js file

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Hello from Tehreem
+        </h1>
+        <p>doing react for first time let's learn it</p>
         <a
           className="App-link"
           href="https://reactjs.org"
